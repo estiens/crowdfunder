169546f22cd46e6bf2afa2244e4bfa5b67f9aa7b
Ahmed Nadar <ahmedfay@hotmail.com>
2013-09-30 17:18:52 -0400

    making test for active navbar element pass

--- spec/features/project_flows_spec.rb
+++ spec/features/project_flows_spec.rb
@@ -11,6 +11,7 @@ describe "Projects Listing" do
 			# got to the page with the projects
 			visit "/projects"
 			expect(current_path).to eq(projects_path)
+
 			expect(page).to have_content("Listing projects")
 			expect(page).to have_content("Project 1")
 			expect(page).to have_content("Project 2")
@@ -18,10 +19,12 @@ describe "Projects Listing" do
 			#click a link to project's show page
 			click_link('Project 1')
       # Expect we're on project1's show page
+
       expect(current_path).to eq(project_path(project1))
       # Expect on this page the first h1 has the text project1's title
-      page.should have_selector('h1:first', text: project1.title)
       expect(page).to have_selector('h1:first', text: project1.title)
+      expect(page).to have_selector('.navbar ul li.active a', text: "Projects")
+
 		end
 
 		it "should display the navigation"  do
@@ -31,8 +34,8 @@ describe "Projects Listing" do
 			expect(current_path).to eq(root_path)
 			expect(page).to have_selector('.navbar ul li.active a', text: "Home")
 
-			click_link "Project 1"
-			expect(page).to have_selector('.navbar ul li.active a', text: "Projects")
+			# click_link "Project 1"
+			# expect(page).to have_selector('.navbar ul li.active a', text: "Projects")
 		end
 	end
 end
