63050b8693b12bdf52a17437e4490687a4a933b5
Ahmed Nadar <ahmedfay@hotmail.com>
2013-09-30 15:44:01 -0400

    writing test for navugations

--- spec/features/project_flows_spec.rb
+++ spec/features/project_flows_spec.rb
@@ -19,4 +19,16 @@ describe "Projects Listing" do
 			expect(page).to have_content("Project 3")
 		end
 	end
+
+	it "should display the navigation"  do
+		visit "/"
+
+		expect(current_path).to eq(root_path)
+		expect(page).to have_selector(".navbar ul li.active a", text: "Home")
+		page.find(".navbar ul").click_link("Projects")
+
+		expect(current_path).to eq(projects_path)
+
+		expect(page).to have_selector('.navbar ul li.active a', text: "Projects")
+	end
 end
