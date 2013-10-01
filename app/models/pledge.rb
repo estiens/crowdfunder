8284ae7b8165cf249cda270bda5e4be632a6fb37
Eric Stiens <estiens@server.fake>
2013-10-01 13:03:21 -0400

    generating pledge model

---
+++ app/models/pledge.rb
@@ -0,0 +1,5 @@
+class Pledge < ActiveRecord::Base
+  attr_accessible :amount, :project, :user_id
+  belongs_to :user
+  belongs_to :project
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
   # attr_accessible :title, :body
   attr_accessible :first_name, :last_name, :email, :password
   has_many :projects
+  has_many :pledges
 
   validates :first_name, :last_name, :email, presence: true
   validates :email, uniqueness: true
