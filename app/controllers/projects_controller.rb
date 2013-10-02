4dd1b087e759e06689692777765e4b96dc877421
Eric Stiens <estiens@server.fake>
2013-10-01 20:57:19 -0400

    tests for my projects passing

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -7,5 +7,28 @@ class ProjectsController < ApplicationController
   def show
     @project=Project.find(params[:id])
   end
+
+  def new
+    @project=Project.new
+  end
+
+  # def create 
+  #   @project = Project.new(params[:project])
+  #   if @project.save
+  #     redirect_to @project
+  #   else
+  #     render :new
+  #   end
+  # end
+
+  # def update
+  #   @project = Project.find(params[:id])
+  #   if @project.update(params)
+  #     redirect_to @project
+  #   else
+  #     render :edit
+  #   end
+  # end
+
   
 end
