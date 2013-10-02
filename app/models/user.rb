1d871b7c96dc4b9c950bc8efae4e3106a7bed00c
Eric Stiens <estiens@server.fake>
2013-10-02 14:10:06 -0400

    working on password confirmation and sorcery

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
   # attr_accessible :title, :body
-  attr_accessible :first_name, :last_name, :email, :password
+  attr_accessible :first_name, :last_name, :email, :password, :password_confirmation
   has_many :projects
   has_many :pledges
 
   validates :first_name, :last_name, :email, presence: true
+  validates_confirmation_of :password, :message => "should match confirmation", :if => :password
+
   # validates :email, uniqueness: true
 end
